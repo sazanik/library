1319e7ed734ae1fd0e3ddebc67d3e9389ebf5ac9
Mikita Sazanchuk <mikit.sazan@gmail.com>
2022-01-21 17:00:03 +0300

    refactor: validation schema for AuthFormSignUp

--- src/components/Auth/Form/SignUp/validation.ts
+++ src/components/Auth/Form/SignUp/validation.ts
@@ -2,23 +2,15 @@ import { TFunction } from 'react-i18next';
 import * as yup from 'yup';
 import { AnyObjectSchema } from 'yup';
 
-import { MAX_LENGTH, MIN_LENGTH } from '../../../../constants';
+import { getSignInSchema } from '../SignIn/validation';
 
 export const getSignUpSchema = (t: TFunction): AnyObjectSchema =>
-  yup.object().shape({
-    email: yup
-      .string()
-      .email()
-      .required(t('errors:required'))
-      .min(MIN_LENGTH.EMAIL, t('errors:minLength') + MIN_LENGTH.EMAIL)
-      .max(MAX_LENGTH.EMAIL, t('errors:maxLength') + MAX_LENGTH.EMAIL),
-    password: yup
-      .string()
-      .required(t('errors:required'))
-      .min(MIN_LENGTH.PASSWORD, t('errors:minLength') + MIN_LENGTH.PASSWORD)
-      .max(MAX_LENGTH.PASSWORD, t('errors:maxLength') + MAX_LENGTH.PASSWORD),
-    confirmPassword: yup
-      .string()
-      .required(t('errors:passwordMismatch'))
-      .oneOf([yup.ref('password'), null], t('errors: passwordMismatch')),
-  });
+  yup
+    .object()
+    .concat(getSignInSchema(t))
+    .shape({
+      confirmPassword: yup
+        .string()
+        .required(t('errors:passwordMismatch'))
+        .oneOf([yup.ref('password'), null], t('errors: passwordMismatch')),
+    });
