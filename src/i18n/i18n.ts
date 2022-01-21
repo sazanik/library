bb23329e0d418cd5208ab6f4c1fd7a6e0375c422
Mikita Sazanchuk <mikit.sazan@gmail.com>
2022-01-21 15:32:02 +0300

    refactor: i18n & locales

--- src/i18n/i18n.ts
+++ src/i18n/i18n.ts
@@ -2,13 +2,7 @@ import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import { i18nextPlugin } from 'translation-check';
 
-import { en } from './locales/en';
-import { ru } from './locales/ru';
-
-const resources = {
-  en,
-  ru,
-};
+import { resources } from './locales/resources';
 
 i18n
   .use(i18nextPlugin)
