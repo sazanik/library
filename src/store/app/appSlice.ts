b8105cd9b0cd9b437e913a1076997266fb14b1cf
Mikita Sazanchuk <mikit.sazan@gmail.com>
2022-01-21 17:39:24 +0300

    feat: add new slices & generic refactor

---
+++ src/store/app/appSlice.ts
@@ -0,0 +1,73 @@
+import { createSlice } from '@reduxjs/toolkit';
+import { AuthError } from 'firebase/auth';
+
+import {
+  createAuthor,
+  getAllAuthors,
+  removeAuthor,
+  updateAuthor,
+} from '../authors/actions';
+import {
+  createBook,
+  getAllBooks,
+  removeBook,
+  updateBook,
+} from '../books/actions';
+import { signInUser, signUpUser } from '../users/actions';
+
+const actionFunctions = [
+  createAuthor,
+  getAllAuthors,
+  removeAuthor,
+  updateAuthor,
+  createBook,
+  getAllBooks,
+  removeBook,
+  updateBook,
+  signInUser,
+  signUpUser,
+];
+
+interface stateProps {
+  loading: boolean;
+  additionalError: string | null;
+}
+
+const initialState: stateProps = {
+  loading: true,
+  additionalError: null,
+};
+
+export const appSlice = createSlice({
+  name: 'app',
+  initialState,
+  reducers: {
+    setLoading: (state) => {
+      state.loading = true;
+    },
+    stopLoading: (state) => {
+      state.loading = false;
+    },
+  },
+  extraReducers: (builder) => {
+    actionFunctions.forEach((f) => {
+      builder.addCase(f.pending, (state) => {
+        state.loading = true;
+      });
+    });
+    actionFunctions.forEach((f) => {
+      builder.addCase(f.fulfilled, (state) => {
+        state.loading = false;
+        state.additionalError = null;
+      });
+    });
+    actionFunctions.forEach((f) => {
+      builder.addCase(f.rejected, (state, action) => {
+        state.additionalError = (action.error as AuthError).message as string;
+        state.loading = false;
+      });
+    });
+  },
+});
+
+export const { setLoading, stopLoading } = appSlice.actions;
---
+++ src/store/authors/actions.ts
@@ -0,0 +1,57 @@
+import { createAsyncThunk } from '@reduxjs/toolkit';
+import {
+  addDoc,
+  collection,
+  deleteDoc,
+  doc,
+  getDoc,
+  getDocs,
+  query,
+  setDoc,
+} from 'firebase/firestore/lite';
+
+import { db } from '../../firebase';
+import { AuthorFormProps, AuthorProps } from '../../types/inerfaces';
+
+const createDoc = async (
+  data: AuthorFormProps
+): Promise<AuthorProps | undefined> => {
+  const docRef = await addDoc(collection(db, 'authors'), data);
+  await setDoc(docRef, { id: docRef.id }, { merge: true });
+  return (await getDoc(docRef)).data() as AuthorProps;
+};
+
+const updateDoc = async (
+  data: AuthorProps
+): Promise<AuthorProps | undefined> => {
+  const docRef = doc(db, 'authors', data.id);
+  await setDoc(docRef, data);
+  return (await getDoc(docRef)).data() as AuthorProps;
+};
+
+const removeDoc = async (id: string): Promise<string | undefined> => {
+  const docRef = doc(db, 'authors', id);
+  await deleteDoc(docRef);
+  return docRef.id;
+};
+
+const readDocs = async (): Promise<AuthorProps[]> => {
+  const collectionRef = query(collection(db, 'authors'));
+  const querySnapshot = await getDocs(collectionRef);
+  const authors: AuthorProps[] = [];
+  querySnapshot.forEach((docItem) => {
+    authors.push(docItem.data() as AuthorProps);
+  });
+  return authors;
+};
+
+export const createAuthor = createAsyncThunk('authors/createAuthor', createDoc);
+
+export const updateAuthor = createAsyncThunk('authors/updateAuthor', updateDoc);
+
+export const removeAuthor = createAsyncThunk('authors/removeAuthor', removeDoc);
+
+export const getAllAuthors = createAsyncThunk(
+  'authors/getAllAuthors',
+  readDocs
+);
